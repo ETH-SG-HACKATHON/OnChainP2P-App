1980617bb000805aa2f8005b7f5632fc8fbbdb98
0xGavinOwen <gavinowen713@gmail.com>
2023-09-10 15:07:10 +0800

    hero layout
    hero layout

---
+++ my-app/src/components/HeroLayout.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import Typewriter from "typewriter-effect";
+import Image from "next/image";
+import btc from "../../public/logo.png";
+
+const HeroLayout = () => {
+  return (
+    <div className="h-[500px] flex justify-between items-center">
+      <div className="ml-[200px] text-xl">
+          <Typewriter
+            options={{
+              strings: ["Hello", "World", "with", "Typewriter.js"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        <h1 className="w-[500px]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ullam
+          molestiae saepe officia ipsa. 
+        </h1>
+      </div>
+
+      <div className="mr-[200px]">
+        <Image src={btc} alt="" width={450} height={450} quality={100} />
+      </div>
+    </div>
+  );
+};
+
+export default HeroLayout;
+
--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Navbar from "@/components/Navbar/Navbar";
+import HeroLayout from "@/components/HeroLayout";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <div>
       <Navbar/>
+      <HeroLayout/>
     </div>
   );
 }
