c8ff8636a5267d268149fb43656b08f9c88d88f7
0xGavinOwen <gavinowen713@gmail.com>
2023-09-10 14:50:37 +0800

    create navbar
    create navbar

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -1,13 +1,13 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Navbar from "@/components/Navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
-      <ConnectButton />
+      <Navbar/>
     </div>
   );
 }
