7163e73e209f877c876bd8e71ddfe43e3cc0c423
0xGavinOwen <gavinowen713@gmail.com>
2023-09-12 00:58:23 +0800

    testing deposit
    testing deposit

--- my-app/src/pages/testing.tsx
+++ my-app/src/pages/testing.tsx
@@ -1,65 +1,13 @@
-// import { getAllListingFromSupabase, getListingFromSupabase } from '@/shared/utils';
-// import React, { useEffect, useState } from 'react';
-// import { useRouter } from "next/router";
+import Navbar from "@/components/Navbar/Navbar"
 
-
-// const ListingsPage: React.FC = () => {
-//   const [listings, setListings] = useState<any[]>([]); 
-//   const router = useRouter();
-
-
-
-//   function handleRouteClick(walletAddress) {
-//     router.push(`/trade/detail/${walletAddress}`);
-//   }
-
-//   useEffect(() => {
-//     async function fetchData() {
-//       try {
-//         const data = await getAllListingFromSupabase();
-
-//         setListings(data);
-//       } catch (error) {
-//         console.error('Error:', error);
-//       }
-//     }
-
-//     fetchData();
-//   }, []); 
-
-//   return (
-//     <div>
-//       <h1>Listings</h1>
-//       <ul>
-//         {listings.map((listing, key) => (
-//           <li key={key}>
-//             <p>Token: {listing.token}</p>
-//             <p>Listing price: {listing.price}</p>
-//             <p>Listing Amount: {listing.amount}</p>
-//             <p>Listing Duration: {listing.duration}</p>
-//             <button onClick={() => handleRouteClick(listing.wallet_address)}>ROUTE</button>
-//             <br />
-//             <br />
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default ListingsPage;
-
-import React from 'react'
-import { useBalance } from 'wagmi'
-
-
-const Testing = () => {
-  const { data, isError, isLoading } = useBalance({
-    address: '0xA0Cf798816D4b9b9866b5330EEa46a18382f251e',
-  })
+const testing = () => {
   return (
-    <div>testing</div>
+    <div>
+      <Navbar/>
+      <h1> deposit</h1>
+      
+      </div>
   )
 }
 
-export default Testing;
+export default testing
