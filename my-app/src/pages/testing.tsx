b298663aef8bd3e88c1a9e45bf552c732737ebbc
0xGavinOwen <gavinowen713@gmail.com>
2023-09-11 19:59:27 +0800

    trade detail 90%
    trade detail 90%

--- my-app/src/pages/testing.tsx
+++ my-app/src/pages/testing.tsx
@@ -1,8 +1,17 @@
 import { getAllListingFromSupabase, getListingFromSupabase } from '@/shared/utils';
 import React, { useEffect, useState } from 'react';
+import { useRouter } from "next/router";
+
 
 const ListingsPage: React.FC = () => {
   const [listings, setListings] = useState<any[]>([]); 
+  const router = useRouter();
+
+
+
+  function handleRouteClick(walletAddress) {
+    router.push(`/trade/detail/${walletAddress}`);
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -28,6 +37,8 @@ const ListingsPage: React.FC = () => {
             <p>Listing price: {listing.price}</p>
             <p>Listing Amount: {listing.amount}</p>
             <p>Listing Duration: {listing.duration}</p>
+            <button onClick={() => handleRouteClick(listing.wallet_address)}>ROUTE</button>
+            <br />
             <br />
           </li>
         ))}
--- my-app/src/pages/trade/detail/[id].tsx
+++ my-app/src/pages/trade/detail/[id].tsx
@@ -1,15 +1,16 @@
 import { BuyerPov } from "@/components/Buyer/BuyerPov";
-import { Button } from "@chakra-ui/react";
-import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useToast } from "@chakra-ui/react";
-import { sendNotificationToSeller } from "@/shared/utils";
+import {
+  getListingFromSupabase,
+  sendNotificationToSeller,
+} from "@/shared/utils";
 import { useAccount } from "wagmi";
 import Navbar from "@/components/Navbar/Navbar";
-import img from "../../../../public/USDC.png";
+import { useRouter } from "next/router";
 
 function BuyerDetailPage() {
-  const [state, setState] = useState(true);
+  const [state, setState] = useState(false);
   const [sellerAddress, setSellerAddress] = useState("");
   const [listingId, setListingId] = useState(0);
   const { address } = useAccount();
@@ -19,48 +20,67 @@ function BuyerDetailPage() {
     if (address)
       await sendNotificationToSeller(sellerAddress, address, listingId);
   };
+
+  const router = useRouter();
+  const { id } = router.query;
+  const [listings, setListings] = useState<any[]>([]); // Adjust the type as needed
+
+  useEffect(() => {
+    // Define an async function inside useEffect to perform the data fetching
+    async function fetchData() {
+      try {
+        const address = "your_wallet_address_here"; // Replace with the desired wallet address
+        const data = await getListingFromSupabase(id);
+
+        // Set the retrieved data to the 'listings' state
+        setListings(data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+
+    // Call the fetchData function to initiate data retrieval when the component is mounted
+    fetchData();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <div className="h-[700px] flex flex-col items-center mt-[50px]">
-        <div className="border-[10px] border-green-main w-[600px] h-[650px] rounded-3xl">
-          <h1 className=" mx-8 my-3 text-2xl font-bold"> Listing details </h1>
-
-          {/* Image */}
-          {/* <div className="flex flex-col justify-center items-center my-[50px]">
-            <Image
-              src={img}
-              width={200}
-              height={200}
-              alt="Image"
-              className="text-center"
-            />
-          </div> */}
+        <div className="border-[10px] border-green-main w-[600px] h-[800px] rounded-3xl">
+          <h1 className=" mx-8 mt-3 text-2xl font-bold"> Listing details </h1>
 
           <div className="p-[40px]">
             {/* Details */}
-            <div className=" flex flex-col gap-5">
-              <h1 className="flex items-center rounded-xl pl-[10px] border-2 w-[500px] h-[40px] ">
-                Wallet Address: 1
-              </h1>
-              <h1 className="flex items-center rounded-xl pl-[10px] border-2 w-[500px] h-[40px] ">
-                Token: Lorem Ipsum
-              </h1>
-              <h1 className="flex items-center rounded-xl pl-[10px] border-2 w-[500px] h-[40px] ">
-                Amount:{" "}
-              </h1>
-              <h1 className="flex items-center rounded-xl pl-[10px] border-2 w-[500px] h-[40px] ">
-                Price:{" "}
-              </h1>
-              <h1 className="flex items-center rounded-xl pl-[10px] border-2 w-[500px] h-[40px] ">
-                Duration:{" "}
-              </h1>
-              <h1 className="flex items-center rounded-xl pl-[10px] border-2 w-[500px] h-[40px] ">
-                Payment Method
-              </h1>
-              <h1 className="flex items-center rounded-xl pl-[10px] border-2 w-[500px] h-[40px] ">
-                Name:{" "}
-              </h1>
+            <div>
+              {listings.map((listing) => (
+                <div key={listing.id} className="flex flex-col gap-5">
+                  <h1 className="flex items-center rounded-xl pl-[10px] border-2 w-[500px] h-[80px] ">
+                    Wallet Address: {listing.wallet_address}
+                  </h1>
+                  <h1 className="flex items-center rounded-xl pl-[10px] border-2 w-[500px] h-[40px] ">
+                    Token: {listing.token}
+                  </h1>
+                  <h1 className="flex items-center rounded-xl pl-[10px] border-2 w-[500px] h-[40px] ">
+                    Amount: {listing.amount}
+                  </h1>
+                  <h1 className="flex items-center rounded-xl pl-[10px] border-2 w-[500px] h-[40px] ">
+                    Price: {listing.price}
+                  </h1>
+                  <h1 className="flex items-center rounded-xl pl-[10px] border-2 w-[500px] h-[40px] ">
+                    Duration: {listing.duration}
+                  </h1>
+                  <h1 className="flex items-center rounded-xl pl-[10px] border-2 w-[500px] h-[40px] ">
+                    Payment Method: {listing.payment_method}
+                  </h1>
+                  <h1 className="flex items-center rounded-xl pl-[10px] border-2 w-[500px] h-[40px] ">
+                    Name: {listing.name}
+                  </h1>
+                  <h1 className="flex items-center rounded-xl pl-[10px] border-2 w-[500px] h-[40px] ">
+                    Account Number: {listing.account_number}
+                  </h1>
+                </div>
+              ))}
             </div>
 
             {/* Button */}
